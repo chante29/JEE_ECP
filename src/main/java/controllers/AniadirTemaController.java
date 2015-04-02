9655fcfced3500d49bf5d5b1c0ae793da1c0bb3d
Chante <Chante@192.168.1.130>
2015-04-02 09:12:22 +0200

    Initial

--- src/main/java/controllers/AniadirTemaController.java
+++ src/main/java/controllers/AniadirTemaController.java
@@ -5,5 +5,5 @@ import es.miw.persistence.models.entities.Tema;
 public interface AniadirTemaController {
 	boolean aniadirTema(Tema tema);
 	
-	boolean existeTema(Tema tema);
+	boolean existeTema(Tema tema); 
 }
