f5f7b709c1bab986db478a3c9894b1c6d9663163
Chante <Chante@192.168.1.130>
2015-04-03 22:39:52 +0200

    Update class NivelEstudiosResource

--- src/main/java/ws/rest/NivelEstudiosResource.java
+++ src/main/java/ws/rest/NivelEstudiosResource.java
@@ -1,11 +1,15 @@
 package ws.rest;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import es.miw.persistence.models.utils.ListaNivelEstudios;
 import es.miw.persistence.models.utils.NivelEstudios;
 import ws.NivelEstudiosUri;
 
@@ -14,7 +18,11 @@ public class NivelEstudiosResource {
 
 	@GET
     @Produces(MediaType.APPLICATION_XML)
-    public NivelEstudios[] findAll() {
-        return NivelEstudios.values();
+    public ListaNivelEstudios findAll() {
+		List<String> nivelesEstudios = new ArrayList<String>();
+		for(NivelEstudios nivelEstudios : NivelEstudios.values()) {
+			nivelesEstudios.add(nivelEstudios.toString());
+		}
+		return new ListaNivelEstudios(nivelesEstudios);
     }
 }
